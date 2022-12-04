83fba4295d0650f1e9face1cb40faa129a3d29f0
Samuel Bihinda <sbihinda@umd.edu>
2022-12-04 18:52:11 -0500

    Updated tweet downloading  configurations

--- download_tweets.py
+++ download_tweets.py
@@ -3,19 +3,22 @@ import json
 import sys
 import time
 import os
-from config import BEARER_TOKEN
+from config import BEARER_TOKEN, LIST_FILE
 
 # Simple script for downloading all the tweets in the list,
 # Get your bearer token from the twitter dev dashborad: https://developer.twitter.com/ (Make an account if you dont have one)
 # and replace Bearer token with it. You can change the file it reads from by running it with an argument, or replacing the list1.txt with your file
 
-# Alan - list 1
-# Samuel - list 2
-# Ansh - list 3
-# John - list 4
-# Syed - list 5
+# You must create a config.py file and include BEARER_TOKEN and a LIST_FILE.
+# Ex: BEARER_TOKEN = "AAAAAA......FkAEA"
+# Ex: LIST_FILE = "list1.txt"
 
-
+# LIST_FILE assignments
+# Alan - list1.txt
+# Samuel - list2.txt
+# Ansh - list3.txt
+# John - list4.txt
+# Syed - list5.txt
 
 
 RATE_LIMIT = 16 * 60 
@@ -25,14 +28,12 @@ MAX_REQ = 850
 queue = []
 
 def bearer_oauth(r):
-    
-   
     r.headers["Authorization"] = f"Bearer {BEARER_TOKEN}"
     r.headers["User-Agent"] = "396HScraper"
+
     return r
 
 def rateLimit():
-
     while queue != [] and time.time() - queue[0] > RATE_LIMIT:
         queue.pop(0)
     
@@ -57,30 +58,21 @@ def apiReq(id):
     else: 
         return temp.json()
 
-
-
-
 if __name__ == '__main__':
     
     tweets = None
 
-    args = sys.argv + ['list1.txt']
+    #args = sys.argv + [sys.argv + LIST_FILE]
 
     data = {}
-    dataFileName = args[1][:-4] + ".json"
+    #dataFileName = [1][:-4] + ".json"
+    dataFileName = LIST_FILE[:-4] + ".json"
 
     print(dataFileName)
-
-    
-
-    
 
     if os.path.exists(dataFileName):
         with open(dataFileName, 'r') as f:
             data = json.load(f)
-
-
-
     
 
     with open(args[1], 'r') as f:
