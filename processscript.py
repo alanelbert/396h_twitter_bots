3f224eb2b1451c1a5318cf490c00f8a4b02b5ec4
Samuel Bihinda <sbihinda@umd.edu>
2022-12-12 20:15:28 -0500

    update

--- processscript.py
+++ processscript.py
@@ -2,25 +2,14 @@ import pandas as pd
 import json
 import numpy as np
 
-
-
-
 if __name__ == '__main__':
-
 
     with open('data/twibot-22.csv', 'r') as f:
         rawcsv = f.read()
-    
-
-    
 
     botidents = {ele.split(',')[0] : ele.split(',')[1] for ele in rawcsv.split('\n')[1:-1]}
     
-    
-
     listfiles = ['data/list1.json', 'data/list2.json', 'data/list5.json']
-
-
 
     print("labels loaded!")
     for lfile in listfiles:
@@ -30,12 +19,9 @@ if __name__ == '__main__':
         labels = []
         text = []
 
-
-
         with open(lfile, 'r') as f:
             data = json.load(f)
-
-        
+    
         for id in data:
             for ele in data[id]:
                 text += [ele['text']]
